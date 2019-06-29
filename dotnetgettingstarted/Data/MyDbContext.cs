4dc172a3826168ad86f1344f24087de8375b9086
Aruna wickramasinghe <arunawickram@gmail.com>
2019-06-29 09:48:58 +0530

    Add entity framework

---
+++ dotnetgettingstarted/Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using dotnetgettingstarted.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetgettingstarted.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<ValueModel> Values { get; set; }
+    }
+}
--- dotnetgettingstarted/Startup.cs
+++ dotnetgettingstarted/Startup.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetgettingstarted.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace dotnetgettingstarted
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MyDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
